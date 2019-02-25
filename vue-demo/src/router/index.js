797b15e7c41c42323931a89f11819eed292ba3c9
lsd <2416711130@qq.com>
2019-02-26 06:20:24 +0800

    vue-demo vuex

--- vue-demo/src/router/index.js
+++ vue-demo/src/router/index.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import SayHi from '@/components/SayHi'
 import Blog from '@/components/Blog'
+import ShowCounter1 from '@/components/ShowCounter1'
+import ShowCounter2 from '@/components/ShowCounter2'
 
 Vue.use(Router)
 
@@ -22,6 +24,16 @@ export default new Router({
       path: '/blog',
       name: 'Blog',
       component: Blog
+    },
+    {
+      path: '/show_counter_1',
+      name: 'ShowCounter1',
+      component: ShowCounter1
+    },
+    {
+      path: '/show_counter_2',
+      name: 'ShowCounter2',
+      component: ShowCounter2
     }
   ]
 })
