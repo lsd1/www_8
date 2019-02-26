66534d278b2d619b4611ab1c1154d538d071ad0b
lsd <2416711130@qq.com>
2019-02-26 12:50:55 +0800

    vuex add

---
+++ vue-demo/src/components/ShowCounter1.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Page one</h1>
+        {{points}}
+        <br />
+        <input type="button" @click="increase" value="点击加1" />
+        <br />
+        <router-link :to="{name: 'ShowCounter2'}">
+            计数页面2
+        </router-link>
+    </div>
+</template>
+
+<script>
+import store from '@/vuex/store'
+import { INCREASE } from '@/vuex/mutation_types'
+export default {
+  computed: {
+    points () {
+      return store.getters.get_points
+    }
+  },
+  methods: {
+    increase () {
+      store.commit(INCREASE, store.getters.get_points + 1)
+    }
+  }
+}
+</script>
